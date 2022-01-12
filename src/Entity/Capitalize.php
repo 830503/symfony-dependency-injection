127b927edea3dca95358376d761d3f6e48be0265
830503 <lixiaoqi19830503@gmail.com>
2022-01-12 16:20:09 +0100

    try another for loop

--- src/Entity/Capitalize.php
+++ src/Entity/Capitalize.php
@@ -8,10 +8,13 @@ class Capitalize implements Transform
 {
     public function transform(string $string): string
     {
-        for ($i = 0; $i < strlen($string); $i++) {
-            if ($i % 2 != 0) {
-                $string[$i] = strtoupper($string[$i]);
-            }
+        // for ($i = 0; $i < strlen($string); $i++) {
+        //     if ($i % 2 != 0) {
+        //         $string[$i] = strtoupper($string[$i]);
+        //     }
+        // }
+        for ($i = 0; $i < strlen($string); $i += 2) {
+            $string[$i] = strtoupper($string[$i]);
         }
         return $string;
     }
